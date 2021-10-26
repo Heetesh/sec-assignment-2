ef8f373c5ed7f1ba38bf37cf5137e393988634e1
Heetesh <yudishdz@gmail.com>
2021-10-26 13:16:31 +0800

    First part partially done -> Str nomalisation left + Date handling

---
+++ TextEditorCore/Application/src/main/java/texteditor/application/FileIO.java
@@ -0,0 +1,70 @@
+package texteditor.application;
+
+import java.awt.*;
+import java.io.*;
+import java.nio.charset.*;
+import java.util.ResourceBundle;
+
+/**
+ * Class responsible for file I/O operations
+ * */
+public class FileIO {
+    private ResourceBundle bundle = null;
+    public FileIO(ResourceBundle bundle) {
+        this.bundle = bundle;
+    }
+
+    public String load(File file, String encoding) throws FileIOException  {
+        // TODO : Loading mechanism
+        StringBuilder fileContent = new StringBuilder();
+
+
+        Charset charset =  Charset.forName(encoding);
+        CharsetDecoder decoder = charset.newDecoder()
+                .onMalformedInput(CodingErrorAction.REPLACE)
+                .onUnmappableCharacter(CodingErrorAction.REPLACE)
+                .replaceWith("?");
+
+        /*TODO: Find out why replaceWith is throwing: Exception in thread "JavaFX Application Thread"
+           java.lang.IllegalArgumentException:
+           Replacement too long at java.base/java.nio.charset.CharsetDecoder.replaceWith(CharsetDecoder.java:291)*/
+        InputStreamReader isr;
+
+        try {
+             isr = new InputStreamReader(
+                    new FileInputStream(file),
+                    decoder
+            );
+
+        } catch (FileNotFoundException e) {
+            throw new FileIOException(this.bundle.getString("fileSaveErrMsg"),e);
+        }
+
+
+        try (BufferedReader br = new BufferedReader(isr)) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                fileContent.append(line);
+            }
+            System.out.println(fileContent.toString());
+            return fileContent.toString();
+        } catch (IOException e) {
+            throw new FileIOException(this.bundle.getString("fileLoadErrMsg"), e);
+        }
+    }
+
+    public void save(File file, String content, String encoding) throws FileIOException {
+
+        Charset charset = Charset.forName(encoding);
+        CharsetEncoder encoder = charset.newEncoder();
+
+        try (PrintWriter pw = new PrintWriter(file, charset)) {
+            pw.write(content);
+            pw.flush();
+        } catch (IOException e) {
+            throw new FileIOException(this.bundle.getString("failSavingToFile"),e);
+        }
+    }
+
+
+}
---
+++ TextEditorCore/Application/src/main/java/texteditor/application/LoadSaveUI.java
@@ -0,0 +1,116 @@
+package texteditor.application;
+
+import javafx.application.Platform;
+import javafx.scene.control.*;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.ResourceBundle;
+
+public class LoadSaveUI {
+    private Stage stage;
+    private FileIO fileIO;
+    private ResourceBundle bundle;
+    private Dialog<String> encodingDialog;
+    private FileChooser fileDialog = new FileChooser();
+    private TextArea textArea;
+
+    private final static int SPACING = 8;
+
+    public LoadSaveUI(Stage stage, FileIO fileIO, ResourceBundle bundle, TextArea textArea) {
+        this.stage = stage;
+        this.fileIO = fileIO;
+        this.bundle = bundle;
+        this.textArea = textArea;
+    }
+
+    // Dialog box to request encoding type from user
+    private String getEncoding() {
+        if (encodingDialog == null) {
+            var encodingComboBox = new ComboBox<String>();
+            var content = new FlowPane();
+            encodingDialog = new Dialog<>();
+            encodingDialog.setTitle(this.bundle.getString("encodingDialog"));
+            encodingDialog.getDialogPane().setContent(content);
+            encodingDialog.getDialogPane().getButtonTypes().addAll(ButtonType.OK, ButtonType.CANCEL);
+            encodingDialog.setResultConverter(
+                    btn -> (btn == ButtonType.OK) ? encodingComboBox.getValue() : null
+            );
+
+            content.setHgap(SPACING);
+            content.getChildren().setAll(new Label("Encoding"), encodingComboBox);
+
+            encodingComboBox.getItems().setAll("UTF-8", "UTF-16", "UTF-32");
+            encodingComboBox.setValue("UTF-8");
+        }
+        return encodingDialog.showAndWait().orElse(null);
+    }
+
+    public void load() {
+        fileDialog.setTitle(this.bundle.getString("UILoadingMessage"));
+
+        File f = fileDialog.showOpenDialog(stage);
+        if (f != null) {
+            String encoding = getEncoding();
+            if(encoding != null) {
+                try {
+                    String content = fileIO.load(f,encoding);
+
+                    Platform.runLater(()-> {
+                        textArea.setText(content);
+                    });
+
+                } catch (FileIOException e) {
+
+                    new Alert(
+                            Alert.AlertType.ERROR,
+                            String.format("Error loading file %s for reason: %s",f.getName(), e.getMessage()),
+                            ButtonType.CLOSE
+                    ).showAndWait();
+                }
+
+            }
+        }
+    }
+
+    public void save(String content) {
+        if (content.length() > 0) {
+
+            fileDialog.setTitle(this.bundle.getString("UISavingMessage"));
+            File f = fileDialog.showSaveDialog(stage);
+            if (f != null) {
+                String encoding = getEncoding();
+
+                // TODO: Save using encoding specified
+                try {
+                        fileIO.save(f, content, encoding);
+                } catch (FileIOException e ) {
+                    new Alert(
+                            Alert.AlertType.ERROR,
+                            String.format("%s", e.getMessage()),
+                            ButtonType.CLOSE
+                    ).showAndWait();
+                }
+            }
+        }
+    }
+
+    /*private Charset getCharset(String unicodeType) {
+        switch (unicodeType) {
+            case "UTF-8":
+                return StandardCharsets.UTF_8;
+                break;
+            case "UTF-16":
+                return StandardCharsets.UTF_16;
+                break;
+            case "UTF-32":
+                return Charset.
+                break;
+        }
+    }*/
+}
